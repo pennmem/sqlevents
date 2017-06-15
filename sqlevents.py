da85d6817e91893f82bf5d19dc858e4d923d934c
Michael V. DePalatis <mike@depalatis.net>
2017-06-15 16:49:37 -0400

    Store stim params in separate table

--- sqlevents.py
+++ sqlevents.py
@@ -1,6 +1,9 @@
 import os.path as osp
+import uuid
 import sqlalchemy as sa
-from sqlalchemy import Table, Column, Boolean, Integer, BigInteger, Numeric, String
+from sqlalchemy import (
+    Table, Column, ForeignKey, Integer, BigInteger, Float, Numeric, String
+)
 import pandas as pd
 
 
@@ -11,6 +14,7 @@ COLUMNS = {
     # FIXME: stim params
     "common": [
         Column('id', Integer, primary_key=True),
+        Column('stim_id', ForeignKey('stim_params.id')),
         Column('subject', String(32), nullable=False, index=True),
         Column('montage', Numeric(2, 1, asdecimal=False), nullable=False),
         Column('experiment', String(32), nullable=False, index=True),
@@ -20,6 +24,24 @@ COLUMNS = {
         Column('eegoffset', BigInteger, nullable=False),
         Column('eegfile', String(128), nullable=False),
         Column('exp_version', String(32)),
+    ],
+
+    # Stimulation parameters
+    "stim": [
+        Column("id", String(32), primary_key=True),
+        # Column("event_id", ForeignKey("events.id")),
+        Column("anode_number", Integer),
+        Column("cathode_number", Integer),
+        Column("anode_label", String(64)),
+        Column("cathode_label", String(64)),
+        Column("amplitude", Float),
+        Column("pulse_freq", Integer),
+        Column("n_pulses", Integer),
+        Column("burst_freq", Integer),
+        Column("n_bursts", Integer),
+        Column("pulse_width", Integer),
+        Column("stim_on", Integer),
+        Column("stim_duration", Integer)
     ],
 
     # FR/catFR-specific columns
@@ -84,7 +106,7 @@ class EventsDatabase(object):
     debug : bool
 
     """
-    __allowed_experiments = ['FR1', 'catFR1', 'PAL1']
+    __allowed_experiments = ['FR1', 'catFR1', 'PAL1', 'PAL2']
 
     def __init__(self, exp_type, engine, meta=None, debug=False):
         assert exp_type in self.__allowed_experiments
@@ -94,7 +116,8 @@ class EventsDatabase(object):
         self._debug = debug
 
         self._tables = {
-            "events": self._make_events_table()
+            "events": self._make_events_table(),
+            "stim_params": Table("stim_params", self.meta, *COLUMNS['stim'])
         }
 
     def __enter__(self):
@@ -134,9 +157,18 @@ class EventsDatabase(object):
             Path to JSON event file to convert.
 
         """
-        columns = [c.name for c in self.events.columns if c.name != 'id']
-        df = pd.read_json(path)[columns]
+        columns = [c.name for c in self.events.columns
+                   if c.name not in ('id', 'stim_id')]
+        events = pd.read_json(path)
+        df = events[columns]
+        stims = [s for s in events.stim_params if s != []]
+        stims_df = pd.DataFrame(stims) if len(stims) > 0 else None
+        if stims_df is not None:
+            stims_df['id'] = [uuid.uuid4().hex for _ in range(len(stims))]
+            df['stim_id'] = stims_df['id']
+
         assert df.experiment[0].lower() == self.exp_type.lower()
+
         if self._debug:
             for n in df.index:
                 try:
@@ -147,16 +179,20 @@ class EventsDatabase(object):
         else:
             df.to_sql('events', self.engine, if_exists='append', index=False)
 
+        if stims_df is not None:
+            stims_df.to_sql('stim_params', self.engine, if_exists='append', index=False)
+
 
 if __name__ == "__main__":
     # Create empty table with SQLAlchemy
     engine = sa.create_engine('sqlite:///events.sqlite')
+    exp = "PAL2"
 
-    with EventsDatabase('PAL1', engine) as db:
+    with EventsDatabase(exp, engine) as db:
         db.create()
 
         # Convert existing JSON events to SQL
-        json_file = osp.expanduser('~/mnt/rhino/protocols/r1/subjects/R1111M/experiments/PAL1/sessions/0/behavioral/current_processed/all_events.json')
+        json_file = osp.expanduser('~/mnt/rhino/protocols/r1/subjects/R1111M/experiments/{:s}/sessions/0/behavioral/current_processed/all_events.json'.format(exp))
         db.from_json(json_file)
 
     print('Easy mode: read events directly with pandas')
@@ -167,10 +203,10 @@ if __name__ == "__main__":
     df = pd.read_sql_query('select * from events where list = 1', engine)
     print(df.head())
 
-    print('Hard mode: query SQLAlchemy-style')
-    with EventsDatabase('FR1', engine) as db:
-        with engine.connect() as conn:
-            s = sa.select([db.events]).where(db.events.c.list == 1)
-            result = conn.execute(s)
-            for row in result:
-                print(row)
+    # print('Hard mode: query SQLAlchemy-style')
+    # with EventsDatabase('FR1', engine) as db:
+    #     with engine.connect() as conn:
+    #         s = sa.select([db.events]).where(db.events.c.list == 1)
+    #         result = conn.execute(s)
+    #         for row in result:
+    #             print(row)
